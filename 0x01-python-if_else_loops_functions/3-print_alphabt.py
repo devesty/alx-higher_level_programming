a545f74f650ca3a053d2b55afa6fefa59711dbba
devesty <estherharriet2@gmail.com>
2022-09-21 06:41:20 -0700

    Print alphabet excepy q and e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for a in range(97, 123):
-    if (chr(a) == 'q' or chr(a) == 'e'):
-            continue
-    else:
-            print("{}".format(chr(a)), end="")
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
