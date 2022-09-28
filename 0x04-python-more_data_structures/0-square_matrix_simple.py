7044318a723cb1cd87957d1ece2e5932076a30f0
devesty <estherharriet2@gmail.com>
2022-09-28 07:42:47 -0700

    Compute the square value of all integers

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    """Compute the square values of all integers of a matrix"""
+    if not matrix:
+        return None
+
+    return list(list(map(lambda a: a*a, num_list)) for num_list in matrix)
