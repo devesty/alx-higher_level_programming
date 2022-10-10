9f1a86b05a39f4efd9351072d88a21d5ec3e98c4
devesty <estherharriet2@gmail.com>
2022-10-10 04:16:57 -0700

    Update

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        result = a/b
+    except (TypeError, ZeroDivisionError):
+        div = None
+    finally:
+        print("Inside result: {}".format(div))
+    return (div)
