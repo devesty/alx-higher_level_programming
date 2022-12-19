c719eabe3345f12fb9d8622dd01be518fe168e29
devesty <estherharriet2@gmail.com>
2022-12-19 03:25:30 -0800

    Updated

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,2 @@
 #!/usr/bin/node
-let firstLine = "C is fun";
-let secondLine = "Python is cool";
-let thirdLine = "JavaScript is amazing";
-console.log(firstLine);
-console.log(secondLine);
-console.log(thirdLine);
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
